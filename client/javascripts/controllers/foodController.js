3dea4d6e4415356caff54ea421beb66be113365d
Mark Donahue <mdonagh@icloud.com>
2015-10-01 14:36:45 -0700

    uses ng show instead of partials, registration is connected to database

--- client/javascripts/controllers/foodController.js
+++ client/javascripts/controllers/foodController.js
@@ -1,9 +1,6 @@
 foodgroups_app.controller('foodgroupsController', function(foodFactory) {
 
 var that = this;
-	this.interests = 0;
-	this.foods = 0;
-	this.diet=0;
 
     this.hello = function(){
     console.log('hello');
@@ -22,14 +19,27 @@ var that = this;
     },
     this.uploaduser = function(){
         console.log(this.user);
+        this.hide=true;
+        this.showinterests=true;
     foodFactory.uploaduser(this.user, function(data){
-    console.log("back at controller");
     console.log(data);
+    that.currentuser = data.insertId;
+    console.log(this.currentuser);
     })
     },
     this.uploadinterests = function(){
-        console.log(this.yourinterests);
+        this.showinterests=false;
+        this.showdiets=true;
+        this.yourinterests.currentuser = that.currentuser
+    console.log(this.yourinterests)
     foodFactory.uploadinterests(this.yourinterests, function(data){
+    console.log(data);
+    })
+    },
+    this.uploaddiets = function(){
+    this.yourdiets.currentuser = that.currentuser
+    console.log(this.yourdiets)
+    foodFactory.uploaddiets(this.yourdiets, function(data){
     console.log(data);
     })
     }
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,18 +1,11 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-<<<<<<< HEAD
   host     : '127.0.0.1',
   user     : 'root',
   password : 'root',
   database : 'mydb',
   port: 3306
-=======
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'mydb'
->>>>>>> b27f283b26c38caa5e8b992bf03b8f89ac15b09f
 });
 
 connection.connect();
@@ -20,19 +13,20 @@ connection.connect();
 var users = require('../controller/users.js')(connection);
 
 module.exports = function (app) {
-<<<<<<< HEAD
 	app.post('/add_user', function (req, res) {
 		users.add(req, res);
 	})
 	app.post('/add_interests', function (req, res) {
 		users.addinterests(req, res);
-=======
+	})
+	app.post('/add_diets', function (req, res) {
+		users.adddiets(req, res);
+	})
 	app
 	.post('/add_user', function (req, res) {
 		users.add(req, res);
 	})
 	.get('/show_groups', function (req, res) {
 		console.log('in route');
->>>>>>> b27f283b26c38caa5e8b992bf03b8f89ac15b09f
 	})
 };
